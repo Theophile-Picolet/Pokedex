320c994da6fe3102f9c95bd3e3357ea35390608f
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2024-12-11 17:26:18 +0100

    quête_4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,5 @@
-function PokemonCard() {
-
-    const pokemon = pokemonList[1];
-    
+function PokemonCard({pokemon}) {
+    console.log(pokemon);
     return (
         <figure>
            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt= {pokemon.name}/> : <p>???</p>}
@@ -9,17 +7,8 @@ function PokemonCard() {
         </figure>
 
     );
+    
 }
 
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 export default PokemonCard;
